5f6fe6231bff5917f70f8dbbe1f09e492e16f9cf
Philip Heilmair <philip.heilmair@gmail.com>
2016-04-18 20:57:53 +0200

    Added shooting bullets

--- src/logic/weapons/Pistol.java
+++ src/logic/weapons/Pistol.java
@@ -1,9 +1,17 @@
 package logic.weapons;
 
 import graphics.lowlevel.Texture;
+import org.joml.Matrix4f;
 import org.joml.Vector2f;
 
-public class Pistol extends Weapon{
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pistol extends Weapon {
+
+    private List<Bullet> bulletList = new ArrayList<>();
+    private Texture sprite;
+    private Vector2f position;
 
     public Pistol(Texture sprite, Vector2f position, float reloadTime, int magazineSize, int roundsPerMinute) {
         super(sprite, position);
@@ -13,5 +21,26 @@ public class Pistol extends Weapon{
         this.magazine = magazineSize;
         this.ammo = magazineSize;
         this.roundsPerMinute = roundsPerMinute;
+        this.sprite = sprite;
+        this.position = position;
+    }
+
+    @Override
+    public void fire(){
+        super.fire();
+        bulletList.add(new Bullet(sprite, position, new Vector2f(1), 10));
+    }
+
+    @Override
+    public void update(){
+
+    }
+
+    @Override
+    public void render(Matrix4f projection){
+        super.render(projection);
+        for(Bullet bullet: bulletList){
+            bullet.render(projection);
+        }
     }
 }
